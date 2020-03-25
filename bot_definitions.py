7561b32347468a3fa154a96585181c78c4f95ab5
A-Jordan-95 <arj14b@my.fsu.edu>
2020-03-25 08:56:50 -0400

    updates and error fixes to gameplay functionality. fixed board output.

--- bot_definitions.py
+++ bot_definitions.py
@@ -4,7 +4,7 @@ import random
 class board:
     def __init__(self):
         self.positions = {1: "_", 2: "_", 3: "_", 4: "_", 5: "_",
-                          6: "_", 7: "_", 8: "_", 9: "_"}
+                          6: "_", 7: " ", 8: " ", 9: " "}
 
     def position_is_empty(self, pos):
         if pos < 7 and pos > 0:
@@ -23,10 +23,9 @@ class board:
 
 
     def get_board(self):
-        #"\n__|___|__\n__|___|__\n  |   |  \n"
         theBoard = str("\n" + self.positions[1] + "_|_" + self.positions[2] + "_|_"
         + self.positions[3] + "\n" + self.positions[4] + "_|_" + self.positions[5] + "_|_"
-        + self.positions[6] + "\n" + self.positions[7] + "_|_" + self.positions[8] + "_|_"
+        + self.positions[6] + "\n" + self.positions[7] + "  |  " + self.positions[8] + "  |  "
         + self.positions[9] + "\n")
         return theBoard
 
@@ -34,13 +33,16 @@ class board:
         num_difs = 0
         for i in range(1,9):
             if (self.positions[i] == 'x' or self.positions[i] == 'X') and \
-            (other_board.positions[i] != 'x' or other_board.positions[i] != 'X'):
+            (other_board.positions[i] != 'x' and other_board.positions[i] != 'X'):
+                print("adding one to num_difs under x...\n")
                 num_difs += 1
             elif (self.positions[i] == 'o' or self.positions[i] == 'O') and \
-            (other_board.positions[i] != 'o' or other_board.positions[i] != 'O'):
+            (other_board.positions[i] != 'o' and other_board.positions[i] != 'O'):
+                print("adding one to num_difs under o...\n")
                 num_difs += 1
             elif (self.positions[i] == '_' or self.positions[i] == ' ') and \
-            (other_board.positions[i] != '_' or other_board.positions[i] != ' '):
+            (other_board.positions[i] != '_' and other_board.positions[i] != ' '):
+                print("adding one to num_difs under _...\n")
                 num_difs += 1
         return num_difs
 
@@ -51,16 +53,32 @@ class board:
             print("Error, invalid position.\n")
 
     def parse_board(self, tweet_text):
+        print("attempting to parse...\n")
+        print("user's tweet:\n" + tweet_text + "\n")
+
         other_board = board()
-        other_board.set_position(1, tweet_text[9])
-        other_board.set_position(2, tweet_text[13])
-        other_board.set_position(3, tweet_text[17])
-        other_board.set_position(4, tweet_text[19])
-        other_board.set_position(5, tweet_text[23])
-        other_board.set_position(6, tweet_text[27])
-        other_board.set_position(7, tweet_text[29])
-        other_board.set_position(8, tweet_text[33])
-        other_board.set_position(9, tweet_text[37])
+        if tweet_text[0] == 'm' or tweet_text[0] == 'M':
+            other_board.set_position(1, tweet_text[9])
+            other_board.set_position(2, tweet_text[13])
+            other_board.set_position(3, tweet_text[17])
+            other_board.set_position(4, tweet_text[19])
+            other_board.set_position(5, tweet_text[23])
+            other_board.set_position(6, tweet_text[27])
+            other_board.set_position(7, tweet_text[29])
+            other_board.set_position(8, tweet_text[35])
+            other_board.set_position(9, tweet_text[41])
+        elif tweet_text[0] == '@':
+            other_board.set_position(1, tweet_text[26])
+            other_board.set_position(2, tweet_text[30])
+            other_board.set_position(3, tweet_text[34])
+            other_board.set_position(4, tweet_text[36])
+            other_board.set_position(5, tweet_text[40])
+            other_board.set_position(6, tweet_text[44])
+            other_board.set_position(7, tweet_text[46])
+            other_board.set_position(8, tweet_text[52])
+            other_board.set_position(9, tweet_text[58])
+        print("Game board extracted from user's tweet:\n" +
+        other_board.get_board() + "\n")
         if self.num_differences(other_board) < 1:
             print("Error: user did not make a move.\n")
             return -1
@@ -91,15 +109,16 @@ class game:
         self.no_move = False
 
     def update_game(self, tweet_text):
-        if self.the_board.parse_board(tweet_text) == 1:
+        ret = self.the_board.parse_board(tweet_text)
+        if ret  == 1:
             #cheat
             self.cheat = True
             return 1
-        elif self.the_board.parse_board(tweet_text) == -1:
+        elif ret == -1:
             #no move
             self.no_move = True
             return -1
-        elif self.the_board.parse_board(tweet_text) == 0:
+        elif ret == 0:
             return 0
 
     def check_combo(self, combo):
@@ -174,10 +193,12 @@ def get_reply_tweet(the_game):
                 reply = str("You cheated! you can only make one move at a time,"
                 + " and can't change any old moves " + the_game.user + "!" +
                 "try again!\n#tweettactoe")
+                the_game.cheat = False
             elif the_game.no_move == True:
                 #nomove
                 reply = str(the_game.user + " , you didn't make a move!"
                 " Try again!\n#tweettactoe")
+                the_game.no_move = False
             else:
                 reply = str("my move: " + the_game.get_board() + "your turn " + the_game.user + "!\n#tweettactoe")
     return reply
@@ -215,6 +236,7 @@ def check_for_and_play_game(api):
         elif "your turn" in (latest_tweet.text) and \
         "#tweettactoe" in (latest_tweet.text) and playing == True:
             if str("@" + latest_tweet.user.screen_name) == newGame.user:
+                print("attempting to update game...\n")
                 if newGame.update_game(latest_tweet.text) == 0:
                     if newGame.game_over() == False:
                         newGame.make_move()
